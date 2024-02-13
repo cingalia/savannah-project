c7c983fdf23eb1fc314547fbd15eeaf26bb59353
Clinton Ingalia <bicxtor1@gmail.com>
2024-02-13 15:10:23 +0300

    updated orders sql

--- sql/orders.sql
+++ sql/orders.sql
@@ -1,8 +1,8 @@
 CREATE TABLE orders (
 	id SERIAL PRIMARY KEY,
-    customerid VARCHAR(50),
-	item VARCHAR(50),
-    description VARCHAR(100),
-    price VARCHAR(25),
-	date  timestamp NOT NULL DEFAULT NOW()
+    customer_id integer REFERENCES customers (id),
+	item VARCHAR(50) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    price integer NOT NULL,
+	created_at TIMESTAMP NOT NULL
 );
