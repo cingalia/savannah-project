dd86ce19e57be2727b7ce92e1976ed0a2af88b30
Clinton Ingalia <bicxtor1@gmail.com>
2024-02-13 13:49:08 +0300

    init go

--- main.go
+++ main.go
@@ -3,8 +3,20 @@ package main
 import (
 	"database/sql"
 	"log"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
+
+var (
+	prefix string = "/api/v1" // API prefix
+	db     *sql.DB
+)
+
+func main() {
+
+	var err error
+	db, err = sql.Open("postgres", "postgres://postgres:postgres@localhost/savannahdb?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
