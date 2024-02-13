867448b2351216d4254cb545e4985886133b831b
Clinton Ingalia <bicxtor1@gmail.com>
2024-02-13 14:10:07 +0300

    created getCustomers

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
@@ -19,4 +21,36 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	router := gin.Default()
+	router.GET("/customers", getCustomers)
+	router.POST("/customers", createCustomer)
+
+	router.Run("localhost:8080")
+}
+
+func getCustomers(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+
+	rows, err := db.Query("SELECT id, firstname, lastname, phone, email FROM customers")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var customer []customer
+	for rows.Next() {
+		var a customer
+		err := rows.Scan(&a.ID, &a.Title, &a.Artist, &a.Price)
+		if err != nil {
+			log.Fatal(err)
+		}
+		customers = append(customers, a)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.IndentedJSON(http.StatusOK, customers)
 }
